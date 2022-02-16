1f518350745ea7161ec7ca336373a6018a126c37
michael <michael@arcturus.co>
2022-02-16 12:39:13 -0500

    added material table bug test

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { default as MaterialTable } from "material-table";
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <MaterialTable columns={[]} data={[]}/>
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { default as MaterialTable,
+    MaterialTableProps,
+    Query,
+    QueryResult,
+    Filter } from "material-table";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App/>
   </React.StrictMode>,
   document.getElementById('root')
 );
